1dbcd85d25262b8731e5e42b0de4d4de18a23615
adonnart <alexandre.donnart@gmail.com>
2018-06-25 15:09:42 +0200

    agenda

--- src/fr/eni/clinique/bo/Agenda.java
+++ src/fr/eni/clinique/bo/Agenda.java
@@ -4,15 +4,30 @@ import java.util.Date;
 
 public class Agenda {
 
+	private Personnel p;
+	private Animal a;
 	private long codeVeto, codeAnimal;
 	private Date dateRdv; // 00 - 15 - 30 - 45 ?
 
-	public Agenda(long codeVeto, long codeAnimal, Date dateRdv) {
+	public Agenda(Personnel p, Animal a, Date dateRdv) {
 		super();
-		this.codeVeto = codeVeto;
-		this.codeAnimal = codeAnimal;
+		this.codeVeto = p.getCodePers();
+		this.codeAnimal = a.getCodeAnimal();
 		this.dateRdv = dateRdv;
 	}
+
+	public Agenda(long codeV, long codeA, Date dateRdv) {
+		super();
+		this.codeVeto = codeV;
+		this.codeAnimal = codeA;
+		this.dateRdv = dateRdv;
+	}
+
+	public Personnel getPersonnel() { return p; }
+	public void setPersonnel(Personnel p) { this.p = p; }
+
+	public Animal getAnimal() { return a; }
+	public void setAnimal(Animal a) { this.a = a; }
 
 	public long getCodeVeto() { return codeVeto; }
 	public void setCodeVeto(long codeVeto) { this.codeVeto = codeVeto; }
@@ -22,5 +37,11 @@ public class Agenda {
 
 	public Date getDateRdv() { return dateRdv; }
 	public void setDateRdv(Date dateRdv) { this.dateRdv = dateRdv; }
+
+	@Override
+	public String toString() {
+		return "Agenda [p=" + p + ", a=" + a + ", codeVeto=" + codeVeto + ", codeAnimal=" + codeAnimal + ", dateRdv="
+				+ dateRdv + "]";
+	}
 
 }
